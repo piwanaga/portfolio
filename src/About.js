a60edd5c8292556d03770344cfb616cf2af86061
phillip <phillip.iwanaga@gmail.com>
2021-11-08 10:27:31 -0800

    updated about

--- src/About.js
+++ src/About.js
@@ -7,9 +7,9 @@ const About = () => {
         <div className='md:grid md:grid-cols-2 h-full'>
             <div className='w-full h-full bg-brown py-28 flex justify-center'>
                 <div className='w-11/12 md:w-11/12 lg:w-4/5 flex flex-col items-center'>
-                    <p className='text-red font-serif text-center text-4xl xl:text-5xl mb-16'>Fullstack software engineer, your future favorite coworker</p>
-                    <p className='sm:w-4/5 md:w-full lg:w-4/5 text-gray text-center font-mono tracking-tighter text-lg mb-12'>I make work fun, I'm great at taking direction and I'm a team player. Currently based in Seattle, but planning to relocate to New York City.</p>
-                    <p className='sm:w-4/5 md:w-full lg:w-4/5 text-gray text-center font-mono tracking-tighter text-lg'> As a software engineer I'm mainly building projects with React, Redux and Node, but I love learning new things and applying creative technical solutions to the task at hand. I'm interested in design, creating meaningful user experiences and delivering high quality, readable code.</p>
+                    <p className='text-red font-serif text-center text-4xl xl:text-5xl mb-16'>Mission-driven software engineer, dedicated team player</p>
+                    <p className='sm:w-4/5 md:w-full lg:w-4/5 text-gray text-center font-mono tracking-tighter text-lg mb-12'>I take ownership of everything I work on, am great at taking direction and feedback and most importantly, I make work fun. Currently based in Seattle, but planning to relocate to New York City.</p>
+                    <p className='sm:w-4/5 md:w-full lg:w-4/5 text-gray text-center font-mono tracking-tighter text-lg'> As an engineer I'm mainly building projects with React, Redux and Node, but I love learning and am happy to identify and apply creative technical solutions to the task at hand. I'm interested in design, creating meaningful user experiences and delivering high quality, readable code.</p>
                 </div>
             </div>
             <div className='hidden md:inline relative'>
